239b98b4bb885ce08c185b1e9b1b8689cf71d8b1
Hülya Nergiz <akin.hulya@outlook.com>
2024-02-14 18:26:37 +0300

    creates Grade class

---
+++ src/main/java/com/workintech/course/entity/Grade.java
@@ -0,0 +1,10 @@
+package com.workintech.course.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+@AllArgsConstructor
+@Data
+public class Grade {
+    private int coefficient;
+    private String note;
+}
